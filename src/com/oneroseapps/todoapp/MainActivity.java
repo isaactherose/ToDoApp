1936f1ed2eb36dd0f855f74802ca82b9fe6b0dfd
isaac rose <isaacfrose@gmail.com>
2014-06-14 20:55:01 -0700

    final to do app

--- src/com/oneroseapps/todoapp/MainActivity.java
+++ src/com/oneroseapps/todoapp/MainActivity.java
@@ -5,8 +5,13 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
@@ -23,12 +28,28 @@ public class MainActivity extends Activity {
        items = new ArrayList<String>();
        itemsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items);
        lvItems.setAdapter(itemsAdapter);
-       items.add("Item 1");
-       items.add("Item 1");
-       items.add("Item 1");
+       items.add("test");
+       deleteItem();
     }
-
-  
+    
+	public void addToDoItem(View v){
+    	//on the add button click, this will happen
+    	//initialize the edit text item
+    	//add that text to the listview
+    	EditText etItem = (EditText) findViewById(R.id.etItem);
+    	itemsAdapter.add(etItem.getText().toString());   
+    	etItem.setText("");
+    }
+	private void deleteItem() {
+    	lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+    		public boolean onItemLongClick(AdapterView<?> aView, View item, int pos, long id){
+    			items.remove(
+    					pos);
+    			itemsAdapter.notifyDataSetInvalidated();
+    			return true;
+    		}
+    	});
+	}
 
 
 	@Override
